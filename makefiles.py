78c5e34fc7d1260df7a3284626c2fda028545e47
SashwatSJain <77226693+SashwatSJain@users.noreply.github.com>
2021-03-31 20:55:39 +0530

    Update makefiles.py

--- makefiles.py
+++ makefiles.py
@@ -1,5 +1,6 @@
-x = 2
+# this code makes 100 .py files in the directory it is run
+x = 1
 while x<=100:
-    a = open(f"/Users/sashwatjain/Code/a-code-a-day/{x}.py", "w")
+    a = open(f"{x}.py", "w")
     a.close
     x+=1
