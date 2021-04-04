5a0a63cdab8f5a86e5ed3e2b0cb5f8445e092f5a
Sashwat Jain <sashwatsjain@gmail.com>
2021-04-04 22:55:20 +0530

    Update 7.py

--- 1-10/7.py
+++ 1-10/7.py
@@ -10,10 +10,12 @@ s5 = int(input("marks in Subject5:-"))
 
 out_of = int(input("maximum marks:"))
 
-print(f"percentage in Subject1 = {s1*100/out_of}")
-print(f"percentage in Subject2 = {s2*100/out_of}")
-print(f"percentage in Subject3 = {s3*100/out_of}")
-print(f"percentage in Subject4 = {s4*100/out_of}")
-print(f"percentage in Subject5 = {s5*100/out_of}")
+print(f"percentage in Subject1 = {s1*100/out_of}%")
+print(f"percentage in Subject2 = {s2*100/out_of}%")
+print(f"percentage in Subject3 = {s3*100/out_of}%")
+print(f"percentage in Subject4 = {s4*100/out_of}%")
+print(f"percentage in Subject5 = {s5*100/out_of}%")
 
 overall_percentage = (s1+s2+s3+s4+s5)*(100)/(out_of*5)
+
+print(f"overall percentage : {overall_percentage}%")
