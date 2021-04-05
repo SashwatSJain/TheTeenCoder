9206e49a46d6ffdfdb188579f6b228b4cfb713a9
Sashwat Jain <sashwatsjain@gmail.com>
2021-04-05 23:16:22 +0530

    Update 8.py

---
+++ 1-10/8.py
@@ -0,0 +1,15 @@
+# 8
+# Tables
+
+x = int(input("which table do u want? "))
+
+print(f"{x}*1 = {x*1}")
+print(f"{x}*2 = {x*2}")
+print(f"{x}*3 = {x*3}")
+print(f"{x}*4 = {x*4}")
+print(f"{x}*5 = {x*5}")
+print(f"{x}*6 = {x*6}")
+print(f"{x}*7 = {x*7}")
+print(f"{x}*8 = {x*8}")
+print(f"{x}*9 = {x*9}")
+print(f"{x}*10 = {x*10}")
